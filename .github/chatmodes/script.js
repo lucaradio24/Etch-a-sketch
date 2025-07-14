f1bb417d00ca0aeff049b822945b37bf04a3ac02
lucaradio24 <lukaabeck3@gmail.com>
2025-07-14 16:15:01 +0200

    Added new features (hover mode, click mode, rainbow mode) and fixed some bugs.

--- .github/chatmodes/script.js
+++ .github/chatmodes/script.js
@@ -1,24 +1,51 @@
 const container = document.querySelector('#grid-container');
-let isClickMode = false;
+let isClickMode = true;
 let isMouseDown = false;
+let isRainbowMode = false;
+let isHoverEnabled = false;
+
+container.addEventListener('click', () => {
+    isHoverEnabled = !isHoverEnabled;
+})
+
+const rgb = function() {
+    let R = Math.round(Math.random() * 255);
+    let G = Math.round(Math.random() * 255);
+    let B = Math.round(Math.random() * 255);
+    return `rgb(${R}, ${G}, ${B})`;
+}
+
+function toggleRGBMode() {
+    isRainbowMode = !isRainbowMode;
+}
 
 function createGrid(size) {
-  container.innerHTML = '';
-    for (let i=0; i < (size * size); i++){
+    container.innerHTML = '';
+    for (let i = 0; i < (size * size); i++) {
         const square = document.createElement('div');
         square.classList.add('grid-square');
         square.style.width = `calc(100% / ${size})`;
         square.style.height = `calc(100% / ${size})`;
-        
-        square.addEventListener('mouseover', () => {
-            if (!isClickMode || isClickMode && isMouseDown){
-            square.style.backgroundColor = 'red';
-            }
-})
-    container.appendChild(square)
+        square.dataset.passes = '0';
 
-}
-}
+        square.addEventListener('mouseover', () => {
+            if (isClickMode && isMouseDown || !isClickMode && isHoverEnabled) {
+                let passes = parseInt(square.dataset.passes);
+                passes++;
+                square.dataset.passes = passes;
+                let opacity = passes * 0.1;
+                if (isRainbowMode) {
+                    square.style.backgroundColor = rgb();
+                } else {
+                    square.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+                }
+            }
+        });
+
+        container.appendChild(square);  
+    }  
+}  
+
 
 document.addEventListener('mousedown', () => isMouseDown = true);
 document.addEventListener('mouseup', () => isMouseDown = false);
